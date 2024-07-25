dd125b13fe70ef35f5fa139a0d4a6e02e6bcd3b1
VetleViking <vetlelind@gmail.com>
2024-07-25 23:52:08 +0200

    made the clock binary

--- clock-test1/app/index.js
+++ clock-test1/app/index.js
@@ -4,20 +4,32 @@ import { preferences } from "user-settings";
 
 const hourselem = document.getElementById("hours")
 const minuteselem = document.getElementById("minutes");
+const backgroundNums = document.getElementById("backgroundNums");
 
 function zeroPad(i) {
     if (i < 10) {
       i = "0" + i;
     }
     return i;
-  };
+};
+
+function numToBin(num) {
+    let bin = num.toString(2);
+    if (bin.length < 6) {
+        let padding = 6 - bin.length;
+        for (let i = 0; i < padding; i++) {
+            bin = "0" + bin;
+        }
+    }
+    return bin;
+}
 
 clock.granularity = "minutes";
 
 clock.ontick = (evt) => {
     let today = evt.date;
-    let hours = today.getHours();
-    let mins = zeroPad(today.getMinutes());
+    let hours = numToBin(today.getHours());
+    let mins = numToBin(today.getMinutes());
     hourselem.text = `${hours}`;
     minuteselem.text = `${mins}`;
 }
