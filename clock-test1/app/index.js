9649be97b55431cb1559dd8a2a06612e270fbe59
VetleViking <vetlelind@gmail.com>
2024-07-25 23:31:46 +0200

    started new clock

--- clock-test1/app/index.js
+++ clock-test1/app/index.js
@@ -1,5 +1,16 @@
 import document from "document";
+import clock from "clock";
+import { preferences } from "user-settings";
 
+const hourselem = document.getElementById("hours")
+const minuteselem = document.getElementById("minutes");
 
-const hello = document.getElementById("hello");
-hello.text = "Hello World!";
+clock.granularity = "minutes";
+
+clock.ontick = (evt) => {
+    let today = evt.date;
+    let hours = today.getHours();
+    let mins = zeroPad(today.getMinutes());
+    hourselem.text = `${hours}`;
+    minuteselem.text = `${mins}`;
+}
